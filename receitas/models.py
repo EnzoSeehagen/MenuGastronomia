51724ba96a01dc49ae6eb2d4eef2638b4b7cf616
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-26 09:16:41 -0300

    Criação da partial 'busca.html' e rafatoração

--- receitas/models.py
+++ receitas/models.py
@@ -13,3 +13,5 @@ class Receita(models.Model):
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
     publicada = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome_receita
