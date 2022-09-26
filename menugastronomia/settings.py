61ecfb5a60e5a480b3ca66e2e11e53d9c9860119
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-26 10:06:38 -0300

    criação do app usuarios e configuração da view

--- menugastronomia/settings.py
+++ menugastronomia/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pessoas',
     'receitas',
+    'usuarios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
