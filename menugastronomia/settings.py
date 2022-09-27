cf92d7dc9d2d359dd0b6d5cfd181cec5c8c02c27
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-26 22:44:44 -0300

    adicionando alertas para verificação de senhas

--- menugastronomia/settings.py
+++ menugastronomia/settings.py
@@ -135,3 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
+from receitas.models import Receita
 
 def cadastro(request):
     if request.method == 'POST':
@@ -13,12 +14,14 @@ def cadastro(request):
         if not email.strip():
             return redirect('cadastro')
         if senha != senha2:
+            messages.error(request, 'As senhas não conferem!')
             return redirect('cadastro')
         if User.objects.filter(email=email).exists():
             return redirect('cadastro')
         user = User.objects.create_user(username=nome, email=email, password=senha)
         user.save()
         print('Usuario cadastrado com sucesso')
+        messages.success(request, 'Cadastro realizado com sucesso!')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
@@ -28,9 +31,7 @@ def login(request):
         email = request.POST['email']
         senha = request.POST['senha']
         if email == "" or senha == "":
-            print('Os campos email e senha não podem ficar em branco')
             return redirect('login')
-        print(email, senha)
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
             user = auth.authenticate(request, username=nome, password=senha)
@@ -46,9 +47,26 @@ def logout(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')
+        id = request.user.id
+        receitas = Receita.objects.filter(pessoa=id)
+        dados = {'receitas' : receitas}
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
 
 def cria_receita(request):
-    return render(request, 'usuarios/cria_receita.html')
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(pessoas=user, nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
+        receita.save()
+        return redirect('dashboard')
+
+    else:
+        return render(request, 'usuarios/cria_receita.html')
